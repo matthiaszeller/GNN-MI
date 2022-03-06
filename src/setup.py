dbdc0b7863c608a3716dd29a7c5d8498562db283
Matthias Zeller <m.zeller1901@gmail.com>
2022-03-06 09:33:48 +0000

    change path management

--- src/setup.py
+++ src/setup.py
@@ -4,8 +4,6 @@ from pathlib import Path
 
 
 def get_data_path():
-    file = Path(__file__).parent.joinpath('data-path.txt')
-    with open(file, 'r') as f:
-        return Path(f.read().strip('\n'))
+    return Path(__file__).parent.parent.joinpath('data')
 
 

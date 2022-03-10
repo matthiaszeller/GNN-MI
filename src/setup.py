35469849185b0af6cbff7454a8a98181ab8e9960
Matthias Zeller <m.zeller1901@gmail.com>
2022-03-10 16:08:50 +0100

    setup: add path routine

--- src/setup.py
+++ src/setup.py
@@ -21,14 +21,19 @@ WANDB_SETTINGS = {
 }
 
 
-def get_data_paths():
+def get_project_root_path():
+    # Hard-coded project structure: data location relative to this setup.py file
+    return Path(__file__).parent.parent
+
+
+def get_data_paths(force_local=False):
     """Return a Path object pointing to the data folders: the reading and writing paths.
     By default, assumes the data path is located at the project root, in which case reading and writing paths are the same
     If you want to override this, create a file `data-path.txt` containing the paths:
     the first list is the reading path, the second line is the writing path."""
     file_data_path = Path(__file__).parent.joinpath('data-path.txt')
     # Parse paths from file
-    if file_data_path.exists():
+    if force_local is False and file_data_path.exists():
         with open(file_data_path, 'r') as f:
             paths = f.read().strip('\n').split('\n')
 
@@ -43,8 +48,7 @@ def get_data_paths():
         return path_in, path_out
 
     logging.info('get_data_path(): using default data folder')
-    # Hard-coded project structure: data location relative to this setup.py file
-    path = Path(__file__).parent.parent.joinpath('data')
+    path = get_project_root_path().joinpath('data')
     return path, path
 
 
